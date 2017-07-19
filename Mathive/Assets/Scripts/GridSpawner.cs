4fa0786bf4aa88b21a4b2aa277bc44e3b451cf7b
HermansJarno <jarnohermans94@gmail.com>
2017-07-19 15:11:41 +0200

    Initial Commit

---
+++ Mathive/Assets/Scripts/GridSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpawner : MonoBehaviour {
+
+  GameObject canvas;
+  Tile[,] Tiles;
+  public GameObject prefabTile;
+  float widthTile, heightTile;
+
+  public void SpawnTiles(int width, int length)
+  {
+    Tiles = new Tile[width, length];
+    for (int i = 0; i < width; i++)
+    {
+      for (int j = 0; j < length; j++)
+      {
+        Vector3 TilePos;
+        if (i % 2 != 0)
+        {
+          TilePos = new Vector3(widthTile + (i * widthTile), heightTile + (j * heightTile), 0);
+        }
+        else
+        {
+          TilePos = new Vector3(widthTile + (i * widthTile), heightTile + (j * heightTile), 0);
+        }
+        GameObject iTile = Instantiate(prefabTile, TilePos, prefabTile.transform.rotation);
+        iTile.transform.SetParent(canvas.transform, false);
+        Tiles[i, j] = new Tile(iTile, 6);
+      }
+    }
+  }
+	// Use this for initialization
+	void Start () {
+    canvas = GameObject.Find("Canvas");
+    RectTransform rt = (RectTransform)prefabTile.transform;
+    widthTile = prefabTile.GetComponent<Image>().sprite.bounds.size.x;
+    heightTile = prefabTile.GetComponent<Image>().sprite.bounds.size.y;
+    SpawnTiles(4, 5);
+
+  }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class Tile
+{
+  public GameObject tileObj;
+  public int number;
+  public Tile(GameObject tileObject, int num)
+  {
+    tileObj = tileObject;
+    number = num;
+    tileObj.transform.Find("Text").GetComponent<Text>().text = num.ToString();
+  }
+}
---
+++ Mathive/Assets/Scripts/TouchController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TouchController : MonoBehaviour {
+
+  public Sprite normalSprite;
+  public Sprite selectedSprite;
+
+  //Change me to change the touch phase used.
+  TouchPhase touchPhase = TouchPhase.Moved;
+  public GraphicRaycaster myGRaycaster;
+
+
+
+  void Update()
+  {
+    //We check if we have more than one touch happening.
+    //We also check if the first touches phase is Ended (that the finger was lifted)
+    if (Input.touchCount > 0 && Input.GetTouch(0).phase == touchPhase)
+    {
+      List<RaycastResult> rayResults = new List<RaycastResult>();
+      PointerEventData ped = new PointerEventData(null);
+      ped.position = Input.GetTouch(0).position;
+      myGRaycaster.Raycast(ped, rayResults);
+      foreach (RaycastResult result in rayResults)
+      {
+        if (result.gameObject.tag == "Hive")
+        {
+          result.gameObject.GetComponent<Image>().sprite = selectedSprite;
+        }
+      }
+    }
+    if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+    {
+      foreach (GameObject Hive in GameObject.FindGameObjectsWithTag("Hive"))
+      {
+        Hive.GetComponent<Image>().sprite = normalSprite;
+      }
+    }
+  }
+}
